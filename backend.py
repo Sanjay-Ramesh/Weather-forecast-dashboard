268ec495398e3e0c00f337ccf322bfb72fe2a3dc
Sanjay-Ramesh <sanjayramesh2111@gmail.com>
2024-01-16 16:16:03 +0530

    Added sky images and stuffs

--- backend.py
+++ backend.py
@@ -3,18 +3,15 @@ import requests
 API_KEY = "1a9b7b3703ece8140d7c35c82d034d52"
 
 
-def get_data(place, forecast_days, kind):
+def get_data(place, forecast_days):
     url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}"
     response = requests.get(url)
     data = response.json()
+    print(data)
     filtered_data = data["list"]
     nr_values = 8 * forecast_days
-    filtered_data = filtered_data[:nr_values]
-    if kind == "Temperature":
-        filtered_data = [dict["main"]["temp"] for dict in filtered_data]
-    if kind == "Sky":
-        filtered_data = [dict["weather"][0]["main"] for dict in filtered_data]    
+    filtered_data = filtered_data[:nr_values]   
     return filtered_data
 
 if __name__ == "__main__":
-    print(get_data(place = "Tokyo",forecast_days=3, kind= "Temperature"))
+    print(get_data(place = "Tokyo",forecast_days=3))
--- home.py
+++ home.py
@@ -2,14 +2,30 @@ import streamlit as st
 import plotly.express as px
 from backend import get_data
 
+# Add title, text input, slider, selectionbox and subheader
 st.title("Weather Forecast for the Next Days")
 place = st.text_input("Place: ")
-days = st.slider("Forecast Days", min_value=1, max_value=5, help="Select the number of forecast days")
+days = st.slider("Forecast Days", min_value=1, max_value=5, 
+                 help="Select the number of forecast days")
 
-option = st.selectbox("Select data to view",("Temperature", "Sky"))
+option = st.selectbox("Select data to view",
+                      ("Temperature", "Sky"))
 st.subheader(f"{option} for the next {days} days in {place}")
 
-d, t = get_data(place, days, option)
+if place:
+    # Get the temperature/sky data
+    filtered_data = get_data(place, days)
 
-figure = px.line(x=d, y=t, labels={"x": "Date", "y": "Temperatures (C)"})
-st.plotly_chart(figure)
+    if option == "Temperature":
+        temperature = [dict["main"]["temp"] for dict in filtered_data]
+        dates = [dict["dt_txt"] for dict in filtered_data]
+        # Create a temp plot
+        figure = px.line(x=dates, y=temperature, labels={"x": "Date", "y": "Temperatures (C)"})
+        st.plotly_chart(figure)
+
+    if option == "Sky":
+        images = {"Clear": "images_weather\\clear.png", "Clouds": "images_weather\\cloud.png",
+                  "Rain": "images_weather\\rain.png", "Snow": "images_weather\\snow.png"}
+        sky_conditions = [dict["weather"][0]["main"] for dict in filtered_data] 
+        image_paths = [images[condition] for condition in sky_conditions]
+        st.image(image_paths, width = 115)
